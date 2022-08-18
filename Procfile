1b3733fa72a2cc9e4c3f7ced5722dc08fa725dee
Soham Shah <31480832+soham2512@users.noreply.github.com>
2022-08-17 22:49:08 -0400

    heroku changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn YFinance.wsgi
+web: gunicorn YFinance.wsgi --log-file -
--- YFinance/settings.py
+++ YFinance/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,7 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR , 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR , 'static')
+# ]
 STATIC_ROOT = BASE_DIR / 'static'
+
+django_heroku.settings(locals())
